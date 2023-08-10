9c6505a3d9cc643aedbf0c49c4cacfd529bf7c28
carinosama <carinosama24@gmail.com>
2023-08-10 14:36:24 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-
-
-int main (void)
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return (0) ;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
