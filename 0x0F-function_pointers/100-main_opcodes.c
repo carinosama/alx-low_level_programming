da1f3f1d1cae7f229cf402622dbd230a75543202
carinosama <carinosama24@gmail.com>
2023-09-13 11:10:56 -0700

    commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -6,17 +6,37 @@
  * @argv: array of arguments
  * Return: Always 0 (Success)
 */
+
 int main(int argc, char *argv[])
 {
-	int j = 0;
+	int b, i;
+	char *arr;
 
-	if (array && size && cmp)
-		while (j < size)
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	b = atoi(argv[1]);
+
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < b; i++)
+	{
+		if (i == b - 1)
 		{
-			if (cmp(array[j]))
-				return (j);
-			j++;
+			printf("%02hhx\n", arr[i]);
+			break;
 		}
-	return (-1);
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
 
